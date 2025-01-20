4c29383b76fbb05c84ce7b57072b049ddbef0828
freekvermaas <freekvermaas@gmail.com>
2025-01-20 13:55:10 +0100

    darkmode page

---
+++ src/app/darkmode/page.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export default function darkmode() {
+  return (
+    <div className="bg-white min-h-screen">
+      <Header />
+      <Footer />
+    </div>
+  )
+}
